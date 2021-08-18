90e2b6a9fa37f2a8b770d611460ed3e01950140a
AlfredoPizana <pizana.alfredo@gmail.com>
2021-08-18 16:35:43 -0500

    Added support to print new posts on dev to

--- backed/src/server.js
+++ backed/src/server.js
@@ -2,11 +2,13 @@
 
 
 const  express = require("express")
+const cors = require('cors')
 const logger = require("./middlewares/logger")
 const usersRouter = require("./routers/users")
 const postsRouter = require("./routers/posts")
 const server = express()
 //middlewars
+server.use(cors())
 server.use(express.json())
 server.use(logger)
 server.use("/users",usersRouter)
